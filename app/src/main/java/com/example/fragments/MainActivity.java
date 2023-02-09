272d0c60c0c5588ee92a991a5e62e10f725e3069
Rahil Essani <bitf19a029@pucit.edu.pk>
2023-02-09 14:32:26 +0500

    Two frags added

--- app/src/main/java/com/example/fragments/MainActivity.java
+++ app/src/main/java/com/example/fragments/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btnOne = findViewById(R.id.buttonFragmentOne);
+        btnTwo = findViewById(R.id.buttonFragmentTwo);
         //linearLayout = findViewById(R.id.layoutMainActivity);
         btnOne.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,9 +27,15 @@ public class MainActivity extends AppCompatActivity {
                 FragmentTransaction transaction =getSupportFragmentManager().beginTransaction();
                 transaction.replace(R.id.layoutMainActivity, fragmentOne);
                 transaction.commit();
+
+            }
+        });
+        btnTwo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
                 ITAfternoonFragment fragmentTwo = new ITAfternoonFragment();
                 FragmentTransaction transactionTwo = getSupportFragmentManager().beginTransaction();
-                transactionTwo.replace(R.id.layoutMainActivity, fragmentTwo);
+                transactionTwo.replace(R.id.layoutMainActivity2, fragmentTwo);
                 transactionTwo.commit();
             }
         });
