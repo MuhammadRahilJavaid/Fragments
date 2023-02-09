9f852198fa1f30e988b4773959a75399a3c7a7b1
Rahil Essani <bitf19a029@pucit.edu.pk>
2023-02-09 13:08:44 +0500

    Fragment Practice

---
+++ app/src/main/java/com/example/fragments/ITAfternoonFragment.java
@@ -0,0 +1,34 @@
+package com.example.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+
+public class ITAfternoonFragment extends Fragment {
+
+    TextView AftIT;
+    public ITAfternoonFragment() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.frag_layout, container, false);
+        AftIT = view.findViewById(R.id.AftITtext);
+        AftIT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AftIT.setText("I'm Changed!BOOM!");
+            }
+        });
+        return view;
+    }
+}
